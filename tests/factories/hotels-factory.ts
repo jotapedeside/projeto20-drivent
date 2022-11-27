7bf43dc69c65e37d8deb8175b5bf07ac0063e8ba
C <jotapedeside@gmail.com>
2022-11-26 22:43:56 -0300

    feature(hotels factory): hotels factory created and added to index

---
+++ tests/factories/hotels-factory.ts
@@ -0,0 +1,23 @@
+import faker from "@faker-js/faker";
+import { prisma } from "@/config";
+
+export async function createHotel() {
+  return prisma.hotel.create({
+    data: {
+      name: faker.name.findName(),
+      image: faker.image.imageUrl(),
+      updatedAt: new Date()
+    }
+  });
+}
+
+export async function createHotelRoom(hotelId: number) {
+  return prisma.room.create({
+    data: {
+      name: faker.name.findName(),
+      capacity: faker.datatype.number(),
+      hotelId,
+      updatedAt: new Date()
+    }
+  });
+}
